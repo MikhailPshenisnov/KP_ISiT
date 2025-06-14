c43ad769a9845ccf402447dbd896bd493f34221c
MikhailPshenisnov <mishail.1703@gmail.com>
2025-06-14 23:42:09 +0300

    Добавление конфигурации
    Добавлены файлы конфигурации для зависимостей и для работы приложения

---
+++ config.py
@@ -0,0 +1,9 @@
+# Токен для бота
+TELEGRAM_TOKEN = "8128728486:AAGP1j8Wg_hiJ33tfhQW9i0MtCkB3uTJW6M"
+
+# Пути к файлам
+DIALOGUES_FILE_PATH = "data/dialogues.txt"
+EMO_DICT_FILE_PATH = "data/kartaslovsent.csv"
+INTENT_DATASET_FILE_PATH = "data/intent_dataset.json"
+MENU_FILE_PATH = "data/menu.json"
+MODEL_FILE_PATH = "models/intent_classifier.pkl"
